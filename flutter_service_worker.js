636fd0945d6d622330eec0f6315f40ceab4c5595
Nadav Halahmi <nadavhalahmi560@gmail.com>
2026-01-15 22:34:09 +0200

    update arroword web app - forgot to bump version in previous commit

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -22,7 +22,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm.wasm": "39dd80367a4e71582d234948adc521c0",
 "favicon.png": "34721e6f9f451d793dbc397239bbcc58",
 "flutter.js": "83d881c1dbb6d6bcd6b42e274605b69c",
-"flutter_bootstrap.js": "3e5eb99cdadc86b8ded43b8113406ff5",
+"flutter_bootstrap.js": "64a92b67096ac5728dfce284f9fd31e8",
 "fonts/KFOlCnqEu92Fr1MmEU9fBBc4.woff2": "115c2d84727b41da5e9b4394887a8c40",
 "fonts/KFOlCnqEu92Fr1MmWUlfBBc4.woff2": "c83e4437a53d7f849f9d32df3d6b68f3",
 "fonts/KFOmCnqEu92Fr1Me4GZLCzYlKw.woff2": "e507bd45228483ae2f864d36f26bb43e",
@@ -32,9 +32,9 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-maskable-512.png": "d21bc477deccb419859f300ca0b5aa98",
 "index.html": "e859a8ece8ebf94a026037c495ae7b30",
 "/": "e859a8ece8ebf94a026037c495ae7b30",
-"main.dart.js": "06bc8f96dfc724e9061f2beacb6ab052",
+"main.dart.js": "0ca211fc972ac115a5eb87fee0759a05",
 "manifest.json": "e0011733c74c2392f28e3e09f3bb6a98",
-"version.json": "43408fbf9eb7fe4e92e4253fdbb9a84b"};
+"version.json": "7da711de38381254e9bf9460bff55a21"};
 // The application shell files that are downloaded before a service worker can
 // start.
 const CORE = ["main.dart.js",
