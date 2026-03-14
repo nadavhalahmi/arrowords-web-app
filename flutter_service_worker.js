d432060b849b1662723845e29c8b0b494d8fedee
Nadav Halahmi <nadavhalahmi560@gmail.com>
2026-03-14 21:44:22 +0200

    update arroword web app - bumped version to 1.0.0+17

--- flutter_service_worker.js
+++ flutter_service_worker.js
@@ -25,7 +25,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "canvaskit/skwasm_heavy.wasm": "b0be7910760d205ea4e011458df6ee01",
 "favicon.png": "34721e6f9f451d793dbc397239bbcc58",
 "flutter.js": "24bc71911b75b5f8135c949e27a2984e",
-"flutter_bootstrap.js": "ba05bdab0ea2ff8a0bcd48d28ef32f67",
+"flutter_bootstrap.js": "8bf65841666c0d31bb06f8815c91ce7f",
 "fonts/KFOlCnqEu92Fr1MmEU9fBBc4.woff2": "115c2d84727b41da5e9b4394887a8c40",
 "fonts/KFOlCnqEu92Fr1MmWUlfBBc4.woff2": "c83e4437a53d7f849f9d32df3d6b68f3",
 "fonts/KFOmCnqEu92Fr1Me4GZLCzYlKw.woff2": "e507bd45228483ae2f864d36f26bb43e",
@@ -35,7 +35,7 @@ const RESOURCES = {"assets/AssetManifest.bin": "693635b5258fe5f1cda720cf224f158c
 "icons/Icon-maskable-512.png": "d21bc477deccb419859f300ca0b5aa98",
 "index.html": "e859a8ece8ebf94a026037c495ae7b30",
 "/": "e859a8ece8ebf94a026037c495ae7b30",
-"main.dart.js": "fc587041fb0382d9b2ea7a44c9780702",
+"main.dart.js": "fef921b508aafb0dee3775ea94bac9e3",
 "manifest.json": "e0011733c74c2392f28e3e09f3bb6a98",
 "version.json": "b928183cc937177426e6fe549ecc4219"};
 // The application shell files that are downloaded before a service worker can
