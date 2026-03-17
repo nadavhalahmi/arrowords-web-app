28779373841b62ea2fc16b4172404221978b6323
Nadav Halahmi <nadavhalahmi560@gmail.com>
2026-03-17 20:14:34 +0200

    update arroword web app - better position of the active cell

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "947307" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "1830625694" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
