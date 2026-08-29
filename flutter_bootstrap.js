cac05ce25073ee6b564e9cb3371f313da9d2d9da
Nadav Halahmi <nadavhalahmi560@gmail.com>
2026-08-29 21:21:20 +0300

    update arroword web app - bug fixes

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3016617598" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2767327880" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
