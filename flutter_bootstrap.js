1ad6bce0befbf0f3e09841caa4eb901615b07589
Nadav Halahmi <nadavhalahmi560@gmail.com>
2026-09-01 22:54:27 +0300

    update arroword web app - try to always open keyboard on web

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -37,6 +37,6 @@ _flutter.buildConfig = {"engineRevision":"e4b8dca3f1b4ede4c30371002441c88c12187e
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4181577712" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
+    serviceWorkerVersion: "2122032810" /* Flutter's service worker is deprecated and will be removed in a future Flutter release. */
   }
 });
